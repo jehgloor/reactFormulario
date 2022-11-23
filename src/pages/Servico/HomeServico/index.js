41c5559895dd6218fdffabdb621d39eb1032805f
thitoGloor <thito_gloor775@hotmail.com>
2022-11-23 20:27:15 -0300

    teste

--- src/pages/Servico/HomeServico/index.js
+++ src/pages/Servico/HomeServico/index.js
@@ -41,8 +41,8 @@ const styles = StyleSheet.create({
   },
   texto: {
     color: "#6A5ACD",
-    fontSize: 26,
-    lineHeight: 46,
+    fontSize: 22,
+    lineHeight: 42,
     fontWeight: "bold",
   },
   botao: {
